52e78b2b2758574c40166e4355af2b97643aa53f
Sandis Kaģis <s20kagisand@venta.lv>
2024-03-15 16:24:55 +0200

    New: class: Driver

---
+++ src/model/Driver.java
@@ -0,0 +1,76 @@
+package model;
+
+public class Driver extends Person{
+
+    //1. variables
+
+    private long dID;
+    private String licenceNo;
+    private float experienceInYears;
+    private static long counter = 0;
+
+    //2. get and set
+
+    public long getdID()
+    {
+        return dID;
+    }
+    public void setdID()
+    {
+        dID = counter;
+        counter++;
+    }
+
+    public String getLicenceNo()
+    {
+        return licenceNo;
+    }
+    public void setLicenceNo(String licenceNo)
+    {
+        if(licenceNo != null && licenceNo.matches("\\d{8}"))
+            this.licenceNo = licenceNo;
+        else
+            this.licenceNo = "Undefined";
+    }
+
+    public float getExperienceInYears()
+    {
+        return experienceInYears;
+    }
+    public void setExperienceInYears(float experienceInYears)
+    {
+        if(experienceInYears >= 0 && experienceInYears <= 120)
+            this.experienceInYears = experienceInYears;
+        else
+            this.experienceInYears = 0;
+    }
+
+    //3. constructors
+
+    public Driver()
+    {
+        setdID();
+        setName("Jānis");
+        setSurname("Ozols");
+        setPersonCode("121290-12311");
+        setLicenceNo("12345678");
+        setExperienceInYears(3);
+    }
+
+    public Driver(String name, String surname, String personCode, String licenceNo, float experienceInYears)
+    {
+        super(name, surname, personCode);
+        setdID();
+        setLicenceNo(licenceNo);
+        setExperienceInYears(experienceInYears);
+    }
+
+    //4. toString
+
+    public String toString()
+    {
+        return dID + ":" + super.toString() + "[" + licenceNo + "  " + experienceInYears + "]";
+    }
+
+    //5. other
+}
--- src/model/Person.java
+++ src/model/Person.java
@@ -1,2 +1,70 @@
-package model;public class Person {
+package model;
+
+public class Person {
+    //1. variables
+    private String name;
+    private String surname;
+    private String personCode;
+
+    //2. get and set
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        if(name != null && name.matches("[A-ZĒŪĪĻĶĢŠĀŽČŅ]{1}[a-zēūīļķģšāžčņ]+"))
+            this.name = name;
+        else
+            this.name = "Undefined";
+
+    }
+
+    public String getSurname()
+    {
+        return surname;
+    }
+
+    public void setSurname(String surname)
+    {
+        if(surname != null && surname.matches("[A-ZĒŪĪĻĶĢŠĀŽČŅ]{1}[a-zēūīļķģšāžčņ]+"))
+            this.surname = surname;
+        else
+            this.surname = "Undefined";
+    }
+    public String getPersonCode()
+    {
+        return personCode;
+    }
+
+    public void setPersonCode(String personCode)
+    {
+        if(personCode != null && personCode.matches("[0-9]{6}-[0-9]{5}"))
+            this.personCode = personCode;
+        else
+            this.personCode = "Undefined";
+    }
+
+    //3. constructors
+    public Person()
+    {
+        setName("Jānis");
+        setSurname("Bērziņš");
+        setPersonCode("123456-76543");
+    }
+
+    public Person(String name, String surname, String personCode)
+    {
+        setName(name);
+        setSurname(surname);
+        setPersonCode(personCode);
+    }
+
+    //4. toString
+    public String toString()
+    {
+        return name + " " + surname + "(" + personCode + ")";
+    }
+    //5. others functions
+
 }
